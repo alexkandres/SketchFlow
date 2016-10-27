044d4dcdacf830b0d83d6ad0ee8ab604ce2f1ced
alexkandres <alexkandres@live.com>
2016-10-27 12:07:35 -0400

    Removed Floating Action bar

--- app/src/main/java/cornjob/sketchflow/MainActivity.java
+++ app/src/main/java/cornjob/sketchflow/MainActivity.java
@@ -1,11 +1,8 @@
 package cornjob.sketchflow;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //message button with snackbar
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //snackbar is similar to a toast
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 }
