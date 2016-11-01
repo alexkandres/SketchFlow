772f618e052ecb1a4714c602520dd337b7e30036
alexkandres <alexkandres@live.com>
2016-11-01 13:28:50 -0400

    Added LoadActivity and LoadFragment and forced landscape orientation

---
+++ app/src/main/java/cornjob/sketchflow/LoadActivity.java
@@ -0,0 +1,20 @@
+package cornjob.sketchflow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class LoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
